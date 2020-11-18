5c00693ae63e026cb7b16608c611a9f878fe89e4
github username <lushiyun@outlook.com>
2020-11-17 21:25:35 -0500

    Add basic structure

---
+++ in-theory-news/components/Header.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <div>
+      this is header
+    </div>
+  )
+};
+
+export default Header;
---
+++ in-theory-news/components/Layout.js
@@ -0,0 +1,25 @@
+// Next's built-in Head component for modifying the <head> of a page
+import Head from 'next/head';
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout = ({ children }) => (
+  <>
+    <Head>
+      <title>In Theory News</title>
+      <meta
+        name="description"
+        content="Simplest news source for your scientific explorations"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div
+      style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <Header />
+      <main style={{ flexGrow: 1 }}>{children}</main>
+      <Footer />
+    </div>
+  </>
+);
+
+export default Layout;
